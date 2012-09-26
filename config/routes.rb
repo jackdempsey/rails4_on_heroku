76a362291257c03c9186bd104f5776e454170b31
Jack Dempsey <jack.dempsey@gmail.com>
2012-09-25 21:58:45 -0700

    setup proper home to welcome

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails4TestApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root to: 'welcome#index'
+  root to: 'home#index'
 
   # Sample of regular route:
   #   get 'products/:id' => 'catalog#view'
