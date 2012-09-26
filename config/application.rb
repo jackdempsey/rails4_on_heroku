08db6c7290d8d704ad61a68c1b8a8ed1d1d7a9e7
Jack Dempsey <jack.dempsey@gmail.com>
2012-09-25 22:08:27 -0700

    fix asset precompiling/db connection issue

--- config/application.rb
+++ config/application.rb
@@ -42,5 +42,6 @@ module Rails4TestApp
 
     # Version of your assets, change this if you want to expire all your assets.
     config.assets.version = '1.0'
+    config.assets.initialize_on_precompile = false
   end
 end
